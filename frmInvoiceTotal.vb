b8a1bc851cc5c8195896b517ba1e1538dd96d784
Oatpapa <marcellsworth@protonmail.com>
2021-12-01 14:57:55 -0500

    Added try catch

--- frmInvoiceTotal.vb
+++ frmInvoiceTotal.vb
@@ -16,76 +16,74 @@
 
         'This below code uses the Decimal.Parse function to convert the string contained in the txtSubtotal.Text to an equivalent decimal
         'data value. If the string can’t be converted, an exception occurs. 
-        Dim subtotal As Decimal = Decimal.Parse(txtSubtotal.Text)
 
-        Dim customerType As String = txtCustomerType.Text
+        Try
+            Dim subtotal As Decimal = Decimal.Parse(txtSubtotal.Text)
 
+            Dim customerType As String = txtCustomerType.Text
 
+            ' The Me statement here functions in a similar way to the "This" statement within Java. Is used to 
+            ' refer to the specific instance of a class that the sub procedure is currently functioning within.
+            ' This sub procedure can change the actual value of discount percent because we passed it ByRef instead of ByVal.
 
-        ' The Me statement here functions in a similar way to the "This" statement within Java. Is used to 
-        ' refer to the specific instance of a class that the sub procedure is currently functioning within.
-        ' This sub procedure can change the actual value of discount percent because we passed it ByRef instead of ByVal.
+            Dim discountPercent As Decimal = Me.ComputeDiscountPercent(customerType, subtotal)
+            Dim discountAmount As Decimal = subtotal * discountPercent
+            Dim invoiceTotal As Decimal = subtotal - discountAmount
 
-        Dim discountPercent As Decimal = Me.ComputeDiscountPercent(customerType, subtotal)
+            'The below three lines of code are all using the FormatX methods, which accept two arguments, the data that is being formatted
+            'and a numerical argument that specifies the precision of the number to be formatted. Because I used ",2", each of the following 
+            'statements are formatted to 2 decimal places.
+            'txtDiscountPercent.Text = FormatPercent(discountPercent, 2)
+            'txtDiscountAmount.Text = FormatCurrency(discountAmount, 2)
+            'txtTotal.Text = FormatCurrency(invoiceTotal, 2)
 
-
-
-
-
-
-
-        Dim discountAmount As Decimal = subtotal * discountPercent
-        Dim invoiceTotal As Decimal = subtotal - discountAmount
-
-        'The below three lines of code are all using the FormatX methods, which accept two arguments, the data that is being formatted
-        'and a numerical argument that specifies the precision of the number to be formatted. Because I used ",2", each of the following 
-        'statements are formatted to 2 decimal places.
-        'txtDiscountPercent.Text = FormatPercent(discountPercent, 2)
-        'txtDiscountAmount.Text = FormatCurrency(discountAmount, 2)
-        'txtTotal.Text = FormatCurrency(invoiceTotal, 2)
-
-        'The below three statements use the ToString method to convert various text strings to their percent form and currency form.
-        'The ToString() method accepts an argument contained within "" 'that determines:
-        '1. The type of formatting (decimal, currency, percent)
-        '2. The precision as a number (two decimal places etc.)
-        txtDiscountPercent.Text = discountPercent.ToString("p2")
-        txtDiscountAmount.Text = discountAmount.ToString("c2")
-        txtTotal.Text = invoiceTotal.ToString("c2")
+            'The below three statements use the ToString method to convert various text strings to their percent form and currency form.
+            'The ToString() method accepts an argument contained within "" 'that determines:
+            '1. The type of formatting (decimal, currency, percent)
+            '2. The precision as a number (two decimal places etc.)
+            txtDiscountPercent.Text = discountPercent.ToString("p2")
+            txtDiscountAmount.Text = discountAmount.ToString("c2")
+            txtTotal.Text = invoiceTotal.ToString("c2")
 
 
-        txtCustomerType.Select()
+            txtCustomerType.Select()
 
 
-        'Incrementing the number of invoices to display the total to the user every time the calculation is run
-        numberOfInvoices = numberOfInvoices + 1
+            'Incrementing the number of invoices to display the total to the user every time the calculation is run
+            numberOfInvoices = numberOfInvoices + 1
 
-        'Keeping a running tally of the total amount of all invoices calculated
-        totalOfInvoices = totalOfInvoices + invoiceTotal
+            'Keeping a running tally of the total amount of all invoices calculated
+            totalOfInvoices = totalOfInvoices + invoiceTotal
 
-        'Keeping a running average of all invoices calculated
-        invoiceAverage = totalOfInvoices / numberOfInvoices
+            'Keeping a running average of all invoices calculated
+            invoiceAverage = totalOfInvoices / numberOfInvoices
 
-        'Display all previously run invoice calculations to the user
-        txtNumberOfInvoices.Text = numberOfInvoices.ToString
-        txtTotalOfInvoices.Text = totalOfInvoices.ToString("c2")
-        txtInvoiceAverage.Text = invoiceAverage.ToString("c2")
+            'Display all previously run invoice calculations to the user
+            txtNumberOfInvoices.Text = numberOfInvoices.ToString
+            txtTotalOfInvoices.Text = totalOfInvoices.ToString("c2")
+            txtInvoiceAverage.Text = invoiceAverage.ToString("c2")
 
-        'Determine the maximum value invoice that has been submitted and displaying it to the user
-        largestInvoice = Math.Max(largestInvoice, invoiceTotal)
-        txtLargestInvoice.Text = largestInvoice.ToString("c2")
+            'Determine the maximum value invoice that has been submitted and displaying it to the user
+            largestInvoice = Math.Max(largestInvoice, invoiceTotal)
+            txtLargestInvoice.Text = largestInvoice.ToString("c2")
 
-        'Determine the minimum value invoice that has been submitted and displaying it to the user
-        smallestInvoice = Math.Min(smallestInvoice, invoiceTotal)
-        txtSmallestInvoice.Text = smallestInvoice.ToString("c2")
+            'Determine the minimum value invoice that has been submitted and displaying it to the user
+            smallestInvoice = Math.Min(smallestInvoice, invoiceTotal)
+            txtSmallestInvoice.Text = smallestInvoice.ToString("c2")
 
-        'Determine the midpoint between the larges and smallest invoices and displaying it to the user
-        midpoint = (smallestInvoice + largestInvoice) / 2
-        txtMidpoint.Text = midpoint.ToString("c2")
+            'Determine the midpoint between the larges and smallest invoices and displaying it to the user
+            midpoint = (smallestInvoice + largestInvoice) / 2
+            txtMidpoint.Text = midpoint.ToString("c2")
 
-
-
-
-
+        Catch ex As InvalidCastException
+            MessageBox.Show("Please check entries for valid numeric data.", ex.GetType.ToString)
+        Catch ex As OverflowException
+            MessageBox.Show("Please ensure that entries aren't too large.", ex.GetType.ToString)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message & vbNewLine & vbNewLine & ex.StackTrace, ex.GetType.ToString)
+        Finally
+            txtCustomerType.Select()
+        End Try
     End Sub
 
 
@@ -131,6 +129,10 @@
                 myDiscountPercent = 0.1D
         End Select
 
+        If myDiscountPercent >= 0.5 Then
+            Throw New Exception("The discount is too big!")
+        End If
+
         Return myDiscountPercent
     End Function
 
@@ -166,7 +168,7 @@
     End Sub
 
 
-    Private Sub ClearAllBoxex(Sender As Object, e As EventArgs) Handles txtCustomerType.DoubleClick, txtSubtotal.DoubleClick
+    Private Sub ClearAllBoxes(Sender As Object, e As EventArgs) Handles txtCustomerType.DoubleClick, txtSubtotal.DoubleClick
 
         txtCustomerType.Text = ""
         txtSubtotal.Text = ""
